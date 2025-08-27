dcb8d8a5e1bdc0d01d8b634052e0e9c7f98a7f64
Daniel Bowers <34407755+DanBowersJR@users.noreply.github.com>
2025-08-27 09:45:15 -0230

    Frontend: externalize API base URL into .env and api.js helper

--- src/App.js
+++ src/App.js
@@ -3,33 +3,36 @@ import Table from './components/Table.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 
+// 👇 centralize base URL
+import API_BASE_URL from './api';
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cities: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            cities: [],
+        };
     }
-  }
 
     componentDidMount() {
-    	fetch('http://localhost:8080/cities')
-    	.then(res => res.json())
-    	.then(json => json)
-    	.then(cities => this.setState({ 'cities': cities }))
+        fetch(`${API_BASE_URL}/api/cities`)
+            .then((res) => res.json())
+            .then((cities) => this.setState({ cities }))
+            .catch((err) => console.error('Error fetching cities:', err));
     }
 
-  render() {
-    return (
-      <div className="App">
-      <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="./">
-                  <img src={logo} alt="logo" width="40" /> City List
-                </a>
-              </nav>
-        <Table cities={ this.state.cities }/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <nav className="navbar navbar-light bg-light">
+                    <a className="navbar-brand" href="./">
+                        <img src={logo} alt="logo" width="40" /> City List
+                    </a>
+                </nav>
+                <Table cities={this.state.cities} />
+            </div>
+        );
+    }
 }
 
 export default App;

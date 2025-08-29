cd10da6e79bd1fae47b991799cff7d4420c2bbb0
Daniel Bowers <34407755+DanBowersJR@users.noreply.github.com>
2025-08-29 07:57:08 -0230

    fixed errors from App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Welcome headline', () => {
+    render(<App />);
+    // Target the <h1> specifically
+    const headline = screen.getByRole('heading', { level: 1, name: /Welcome to FlightGrid/i });
+    expect(headline).toBeInTheDocument();
 });
